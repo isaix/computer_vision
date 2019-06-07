b71f7099f6910708e09699a6df88c6feb0e8ab80
Y0SSB0SS <josephine.mellin@gmail.com>
2019-06-07 12:12:19 +0200

    yes

--- JAvaFXProject/src/objrecognition/HoughCirclesRun.java
+++ JAvaFXProject/src/objrecognition/HoughCirclesRun.java
@@ -11,51 +11,30 @@ import javafx.scene.image.ImageView;
 
 
 public class HoughCirclesRun {
-	
-
-    public Mat run(Mat frame) {
-    	
-  
-        // Load an image
-        // Check if image is loaded fine
-        if( frame.empty() ) {
-            System.out.println("Error opening image!");
-            System.out.println("Program Arguments: [image_name -- default "
-                     +"] \n");
-            System.exit(-1);
-        }
 
 
+	public Mat run(Mat frame) {
 
-         Mat gray = new Mat();
+		Mat gray = new Mat();
+		Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
 
+		Imgproc.medianBlur(gray, gray, 5);
+		Mat circles = new Mat();
+		Imgproc.HoughCircles(gray, circles, Imgproc.HOUGH_GRADIENT, 1.0,
+				(double)gray.rows()/16, // change this value to detect circles with different distances to each other
+				100.0, 30.0, 1, 30); // change the last two parameters
+		// (min_radius & max_radius) to detect larger circles
+		for (int x = 0; x < circles.cols(); x++) {
+			double[] c = circles.get(0, x);
+			Point center = new Point(Math.round(c[0]), Math.round(c[1]));
+			// circle center
+			Imgproc.circle(frame, center, 1, new Scalar(0,100,100), 3, 8, 0 );
+			// circle outline
+			int radius = (int) Math.round(c[2]);
+			Imgproc.circle(frame, center, radius, new Scalar(255,0,255), 3, 8, 0 );
+		}
 
-         Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+		return frame;
 
-        Imgproc.medianBlur(gray, gray, 5);
-        Mat circles = new Mat();
-        Imgproc.HoughCircles(gray, circles, Imgproc.HOUGH_GRADIENT, 1.0,
-                (double)gray.rows()/16, // change this value to detect circles with different distances to each other
-                100.0, 30.0, 1, 30); // change the last two parameters
-                // (min_radius & max_radius) to detect larger circles
-        for (int x = 0; x < circles.cols(); x++) {
-            double[] c = circles.get(0, x);
-            Point center = new Point(Math.round(c[0]), Math.round(c[1]));
-            // circle center
-            Imgproc.circle(frame, center, 1, new Scalar(0,100,100), 3, 8, 0 );
-            System.out.println("center: " + center);
-            // circle outline
-            int radius = (int) Math.round(c[2]);
-            System.out.println("radius: " +radius );
-            
-            System.out.println("in frame: " + frame);
-            Imgproc.circle(frame, center, radius, new Scalar(255,0,255), 3, 8, 0 );
-        }
-
-        
-        return frame;
-        
-        
-     
-    }    
+	}    
 }
--- JAvaFXProject/src/objrecognition/HoughLinesRun.java
+++ JAvaFXProject/src/objrecognition/HoughLinesRun.java
@@ -1,48 +1,34 @@
 package objrecognition;
 
-
 import org.opencv.core.*;
 import org.opencv.core.Point;
-import org.opencv.highgui.HighGui;
 import org.opencv.imgproc.Imgproc;
 
 class HoughLinesRun {
 
-    public Mat runLine(Mat frame) {
-        // Declare the output variables
-        Mat dst = new Mat(), cdst = new Mat(), cdstP;
+	public Mat runLine(Mat frame) {
+		// Declare the output variables
+		Mat dst = new Mat(), cdst = new Mat(), cdstP;
 
-        // Check if image is loaded fine
-        if( frame.empty() ) {
-            System.out.println("Error opening image!");
-            System.out.println("Program Arguments: [image_name -- default "
-                 );
-            System.exit(-1);
-        }
-        
-        //! [edge_detection]
-        // Edge detection
-        Imgproc.Canny(frame, dst, 50, 200, 3, false);
-        //! [edge_detection]
+		// Edge detection
+		Imgproc.Canny(frame, dst, 50, 200, 3, false);
+		//! [edge_detection]
 
-        // Copy edges to the images that will display the results in BGR
-        Imgproc.cvtColor(dst, cdst, Imgproc.COLOR_GRAY2BGR);
-        cdstP = cdst.clone();
+		// Copy edges to the images that will display the results in BGR
+		Imgproc.cvtColor(dst, cdst, Imgproc.COLOR_GRAY2BGR);
+		cdstP = cdst.clone();
 
-
-        // Probabilistic Line Transform
-        Mat linesP = new Mat(); // will hold the results of the detection
-        Imgproc.HoughLinesP(dst, linesP, 1, Math.PI/180, 50, 50, 10); // runs the actual detection
-        for (int x = 0; x < linesP.rows(); x++) {
-            double[] l = linesP.get(x, 0);
-            Imgproc.line(frame, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(0, 0, 255), 3, Imgproc.LINE_AA, 0);
-        }
-           
+		// Probabilistic Line Transform
+		Mat linesP = new Mat(); // will hold the results of the detection
+		Imgproc.HoughLinesP(dst, linesP, 1, Math.PI/180, 50, 50, 10); // runs the actual detection
+		for (int x = 0; x < linesP.rows(); x++) {
+			double[] l = linesP.get(x, 0);
+			Imgproc.line(frame, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(0, 0, 255), 3, Imgproc.LINE_AA, 0);
+		}
 
 
-        
 		return frame;
-       
-    }
-   
+
+	}
+
 }
