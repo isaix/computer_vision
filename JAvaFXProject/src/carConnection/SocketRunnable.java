f2ab926109038bf366c6da1b49629d2831b382e4
Frederik Husted <fredehus@gmail.com>
2019-06-16 14:58:25 +0200

    LInes

--- JAvaFXProject/src/carConnection/SocketRunnable.java
+++ JAvaFXProject/src/carConnection/SocketRunnable.java
@@ -32,19 +32,19 @@ public class SocketRunnable implements Runnable{
 		while(connected) {
 			
 			
-			try {
-				ArrayList<Point> foundWalls2 = ColorDetector.run();
-			} catch(Exception e){
-				System.out.println("Intet frame, start kameraet.");				
-			}
-			
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
+//			try {
+//				ArrayList<Point> foundWalls2 = ColorDetector.run();
+//			} catch(Exception e){
+//				System.out.println("Intet frame, start kameraet.");				
+//			}
+//			
+//			try {
+//				Thread.sleep(2000);
+//			} catch (InterruptedException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//			
 			
 			ArrayList<Point> points = HoughCirclesRun.getvalidBallCoordinates();
 			if(points == null || points.isEmpty()) {
