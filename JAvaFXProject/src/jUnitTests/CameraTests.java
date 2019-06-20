a63bc3bcb7831ffb0dcbb222e3321ccf24aa301f
Isaac Irani <isaac@eywasystems.dk>
2019-06-20 11:19:28 +0200

    new tests added

--- JAvaFXProject/src/jUnitTests/CameraTests.java
+++ JAvaFXProject/src/jUnitTests/CameraTests.java
@@ -11,14 +11,13 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import objrecognition.CarDetector;
 import objrecognition.CircleDetector;
 
 public class CameraTests {
 
-	private CircleDetector houghCirclesRun = new CircleDetector();
-	
-	private ScheduledExecutorService timer;
-
+	private CircleDetector circleDetector = new CircleDetector();
+	private CarDetector carDetector = new CarDetector();
 
 
 	@BeforeClass
@@ -32,80 +31,155 @@ public class CameraTests {
 
 
 	@Test
-	public void testImageRecognition1() {	
+	public void testBallRecognition1() {	
 		nu.pattern.OpenCV.loadShared();
 
 		System.out.println("test one");
 		//Reading the Image from the file  
 		String file ="./testAssets/image1-0.jpg"; 
 		Mat image = Imgcodecs.imread(file); 
-		
+
 		//Imgproc.resize(image, image, new Size(1080, 1920), 0, 0, Imgproc.INTER_AREA);
 
 		if( image.empty() )  {
 			System.out.println("empty");
 		} else {
 			for(int i = 0; i < 100 ; i++) {
-			
-				houghCirclesRun.run(image);
-				 file ="./testAssets/image1-" + i + ".jpg"; 
-				 image = Imgcodecs.imread(file); 
+
+				circleDetector.run(image);
+				file ="./testAssets/image1-" + i + ".jpg"; 
+				image = Imgcodecs.imread(file); 
 			}
 		}
-		
-		assertEquals(10, houghCirclesRun.getvalidBallCoordinates().size());
+
+		assertEquals(10, circleDetector.getvalidBallCoordinates().size());
 	}
-	
+
 
 	@Test
-	public void testImageRecognition2() {	
+	public void testBallRecognition2() {	
 		nu.pattern.OpenCV.loadShared();
 
 		System.out.println("test one");
 		//Reading the Image from the file  
 		String file ="./testAssets/image2-0.jpg"; 
 		Mat image = Imgcodecs.imread(file); 
-		
+
 		//Imgproc.resize(image, image, new Size(1080, 1920), 0, 0, Imgproc.INTER_AREA);
 
 		if( image.empty() )  {
 			System.out.println("empty");
 		} else {
 			for(int i = 0; i < 100 ; i++) {
-			
-				houghCirclesRun.run(image);
-				 file ="./testAssets/image1-" + i + ".jpg"; 
-				 image = Imgcodecs.imread(file); 
+
+				circleDetector.run(image);
+				file ="./testAssets/image1-" + i + ".jpg"; 
+				image = Imgcodecs.imread(file); 
 			}
 		}
-		
-		assertEquals(10, houghCirclesRun.getvalidBallCoordinates().size());
+
+		assertEquals(10, circleDetector.getvalidBallCoordinates().size());
 	}
-	
+
 
 	@Test
-	public void testImageRecognition3() {	
+	public void testBallRecognition3() {	
 		nu.pattern.OpenCV.loadShared();
 
 		System.out.println("test one");
 		//Reading the Image from the file  
 		String file ="./testAssets/image3-0.jpg"; 
 		Mat image = Imgcodecs.imread(file); 
-		
+
 		//Imgproc.resize(image, image, new Size(1080, 1920), 0, 0, Imgproc.INTER_AREA);
 
 		if( image.empty() )  {
 			System.out.println("empty");
 		} else {
 			for(int i = 0; i < 100 ; i++) {
-			
-				houghCirclesRun.run(image);
-				 file ="./testAssets/image1-" + i + ".jpg"; 
-				 image = Imgcodecs.imread(file); 
+
+				circleDetector.run(image);
+				file ="./testAssets/image1-" + i + ".jpg"; 
+				image = Imgcodecs.imread(file); 
 			}
 		}
-		
-		assertEquals(10, houghCirclesRun.getvalidBallCoordinates().size());
+
+		assertEquals(10, circleDetector.getvalidBallCoordinates().size());
+	}
+
+	@Test
+	public void testCarRecognition1() {	
+		nu.pattern.OpenCV.loadShared();
+
+		System.out.println("test one");
+		//Reading the Image from the file  
+		String file ="./testAssets/image1-0.jpg"; 
+		Mat image = Imgcodecs.imread(file); 
+
+		//Imgproc.resize(image, image, new Size(1080, 1920), 0, 0, Imgproc.INTER_AREA);
+
+		if( image.empty() )  {
+			System.out.println("empty");
+		} else {
+			for(int i = 0; i < 100 ; i++) {
+
+				carDetector.run(image);
+				file ="./testAssets/image1-" + i + ".jpg"; 
+				image = Imgcodecs.imread(file); 
+			}
+		}
+
+		assertEquals(3, carDetector.getvalidCarCoordinates().size());
+	}
+	
+	@Test
+	public void testCarRecognition2() {	
+		nu.pattern.OpenCV.loadShared();
+
+		System.out.println("test one");
+		//Reading the Image from the file  
+		String file ="./testAssets/image2-0.jpg"; 
+		Mat image = Imgcodecs.imread(file); 
+
+		//Imgproc.resize(image, image, new Size(1080, 1920), 0, 0, Imgproc.INTER_AREA);
+
+		if( image.empty() )  {
+			System.out.println("empty");
+		} else {
+			for(int i = 0; i < 100 ; i++) {
+
+				carDetector.run(image);
+				file ="./testAssets/image2-" + i + ".jpg"; 
+				image = Imgcodecs.imread(file); 
+			}
+		}
+
+		assertEquals(3, carDetector.getvalidCarCoordinates().size());
+	}
+	
+	@Test
+	public void testCarRecognition3() {	
+		nu.pattern.OpenCV.loadShared();
+
+		System.out.println("test one");
+		//Reading the Image from the file  
+		String file ="./testAssets/image3-0.jpg"; 
+		Mat image = Imgcodecs.imread(file); 
+
+		//Imgproc.resize(image, image, new Size(1080, 1920), 0, 0, Imgproc.INTER_AREA);
+
+		if( image.empty() )  {
+			System.out.println("empty");
+		} else {
+			for(int i = 0; i < 100 ; i++) {
+
+				carDetector.run(image);
+				file ="./testAssets/image3-" + i + ".jpg"; 
+				image = Imgcodecs.imread(file); 
+			}
+		}
+
+		assertEquals(3, carDetector.getvalidCarCoordinates().size());
 	}
 
 }
